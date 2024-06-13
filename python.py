f73d319dbc73ad11f47ba2881deb56ce4604da3c
ijogun <ijogunseyi@gmail.com>
2024-06-13 12:08:43 -0400

    reintroduced changes

--- python.py
+++ python.py
@@ -1,9 +1,5 @@
-#sample python code
+sample python code
 x = [1,2,3,4,5,6,7,8]
 for i in x:
 	print (f" The list of the numbers above are {i}")
-
-print ("Hello World")
-
-print("code does have a bug")
 
